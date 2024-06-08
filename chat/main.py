be11283e31264364adef698ae4c2d4127124108b
Gilbert Gómez <gilbertgomez@lahaus.com>
2024-06-07 23:47:04 -0400

    chat changes

--- chat/main.py
+++ chat/main.py
@@ -21,44 +21,30 @@ def encode_image(image_path):
 
 @cl.on_message
 async def on_message(msg: cl.Message):
-    # if not msg.elements:
-    #     await cl.Message(content="No file attached").send()
-    #     return
-
-    # Processing images exclusively
-
-
     encoded_images = []
     for file in msg.elements:
         if "image" in file.mime:
             encoded_images.append(encode_image(image_path=file.path))
-    print(encoded_images[0])
     response = call_langchain(msg.content, encoded_images)
-    images = response.json().get("response").get("context").get("images")
-    front_images = []
-    images_names = []
-    for idx, value in enumerate(images):
-        extension = value.split(";base64")[0].split("/")[1]
-        extensionpssible = value.split(";base64,")[1]
-        decoded_bytes = base64.b64decode(extensionpssible)
-        images_names.append(f"./imgs/{idx}.{extension}")
-        with open(f"{IMAGES_PATH}/{idx}.{extension}", "wb") as image_file:
-            image_file.write(decoded_bytes)
-        front_images.append(cl.Image(path=f"{IMAGES_PATH}/{idx}.{extension}", name=f"{idx}.{extension}", display="inline", size="small"))
     if response.status_code == 200:
+        data = response.json().get("response")
+        for key, value in data.items() :
+            print (key)
+        images = data.get("context").get("images")
+        front_images = []
+        images_names = []
+        for idx, value in enumerate(images):
+            extension = value.split(";base64")[0].split("/")[1]
+            extensionpssible = value.split(";base64,")[1]
+            decoded_bytes = base64.b64decode(extensionpssible)
+            images_names.append(f"./imgs/{idx}.{extension}")
+            with open(f"{IMAGES_PATH}/{idx}.{extension}", "wb") as image_file:
+                image_file.write(decoded_bytes)
+            front_images.append(cl.Image(path=f"{IMAGES_PATH}/{idx}.{extension}", name=f"{idx}.{extension}", display="inline", size="small"))
+    
         await cl.Message(
-            content=response.json().get("response").get("response"),
+            content=data.get("response"),
             elements=front_images
             ).send()
         return
     
-    # print(encoded_images)
-        
-    # images = [file for file in msg.elements if "image" in file.mime]
-
-    # Read the first image
-    # with open(images[0].path, "r") as f:
-    #     pass
-
-    # await cl.Message(content=f"Received {len(images)} image(s)").send()
-
